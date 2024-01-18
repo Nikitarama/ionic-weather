c444b59b9a1e3a8f105cfa3e99ce90909eed0f3b
Nikitarama <crazyscientist996@gmail.com>
2024-01-18 11:41:51 +0200

    third

--- services/weather.service.ts
+++ services/weather.service.ts
@@ -15,7 +15,7 @@ export class WeatherService {
     let lat = -33.918861;
     let lon = 18.423300;
     let apiID = '9de081b04588e26fbd72269dbdf8da6d';
-    let queryString = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiID}`;
+    let queryString = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=45&appid=${apiID}&units=metric`;
     //using the queryString to pull the info from the API//
 
     return this.http.get(queryString);
