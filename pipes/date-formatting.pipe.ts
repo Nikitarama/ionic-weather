a569daaca10ec0daf3d668a8f58e5a313ab5e6ac
Nikitarama <crazyscientist996@gmail.com>
2024-01-24 00:11:17 +0200

    6th

--- pipes/date-formatting.pipe.ts
+++ pipes/date-formatting.pipe.ts
@@ -5,8 +5,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateFormattingPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(unixTimestamp: string[]): unknown {
+    let a = new Date(+unixTimestamp * 1000 );
+    let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+    let days = ['Sun', 'Mon', 'Tues', 'Wed', 'Fri', 'Sat'];
+
+    return days[a.getDay()] + ', ' + a.getDate() + ' ' + months[a.getMonth()] + ' ' + a.getHours() + ':' + a.getMinutes() + '0';
   }
 
 }
